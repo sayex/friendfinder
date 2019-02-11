1b21113a8886a6ba2f7058a33e363123980b9d9c
sayex <elsayer@msn.com>
2019-02-11 13:46:16 -0700

    added readme and more friends.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # friendfinder
+
+## Create a Node Server and use express to pass a form info to the backend and return some data to the front end
+
+Using the example and class repo I recreated the site for this activity.
+
+### Overview
+
+In this activity, you'll build a compatibility-based "FriendFinder" application -- basically a dating app. This full-stack site will take in results from your users' surveys, then compare their answers with those from other users. The app will then display the name and picture of the user with the best overall match.
+
+You will use Express to handle routing. Make sure you deploy your app to Heroku so other users can fill it out.
+
+### Tehcnology used
+
+* Node.js
+* express
+* Bootstrap
+* FontAwsome
+* Chosen.js
+
+![ff](/app/assets/ff.png)
--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,22 @@
 
 var friendsArray = [{
     name: "Paula Smith",
-    photo: "https://ichef.bbci.co.uk/news/660/cpsprodpb/37B5/production/_89716241_thinkstockphotos-523060154.jpg",
+    photo: "https://static.wixstatic.com/media/2d23a2_83f78663a28a4152a5125a3dcf43e726.jpg/v1/fill/w_546,h_745,al_c,lg_1,q_85/2d23a2_83f78663a28a4152a5125a3dcf43e726.webp",
     scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
 }, {
     name: "Sally Frank",
     photo: "https://media1.tenor.com/images/5c406b927ec59a31eb67e3366f3121ef/tenor.gif?itemid=11909469",
     scores: ["3", "1", "3", "3", "4", "1", "1", "3", "5", "5"]
-}];
+}, {
+    name: "Shawn House",
+    photo: "http://pm1.narvii.com/5866/76b201e12343e860570c62f4067307e04ed545fe_00.jpg",
+    scores: ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1"]
+}, {
+    name: "Mary Poppins",
+    photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Mary_Poppins5.jpg/250px-Mary_Poppins5.jpg",
+    scores: ["5", "5", "5", "5", "5", "5", "5", "5", "5", "5"]
+}
+
+];
 
 module.exports = friendsArray;
