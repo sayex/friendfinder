297e4f0ea5cb6dd97b84d6bf02cc9fe2ed51c9f6
sayex <elsayer@msn.com>
2019-02-10 18:18:40 -0700

    fixed model

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,21 +1,10 @@
-// ===============================================================================
-// LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// ===============================================================================
+
 
 var friendsData = require("../data/friends");
 
 
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
 module.exports = function (app) {
-    // API GET Requests
-    // Below code handles when users "visit" a page.
-    // In each of the below cases when a user visits a link
-    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
-    // ---------------------------------------------------------------------------
+
 
     app.get("/api/friends", function (req, res) {
         res.json(friendsData);
@@ -31,11 +20,15 @@ module.exports = function (app) {
         };
         var userScore = req.body.scores;
         var totalDifference = 0;
+
         for (var i = 0; i < friendsData.length; i++) {
             totalDifference = 0;
+
             for (var j = 0; j < friendsData[i].scores[j]; j++) {
                 totalDifference += Math.abs(parseInt(userScore[j] - friendsData[i].scores[j]))
+
                 if (totalDifference <= match.difference) {
+
                     match.name = friendsData[i].name;
                     match.photo = friendsData[i].photo;
                     match.difference = totalDifference;
@@ -50,16 +43,4 @@ module.exports = function (app) {
 
     });
 
-    // ---------------------------------------------------------------------------
-    // I added this below code so you could clear out the table while working with the functionality.
-    // Don"t worry about it!
-
-    app.post("/api/clear", function (req, res) {
-        // Empty out the arrays of data
-        friendsData.length = [];
-
-        res.json({
-            ok: true
-        });
-    });
 };
